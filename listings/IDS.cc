7e286cd2d77ab59be21d8052e5a7ac1fdd7dad07
csosgic <tempo_github@proton.me>
2024-05-19 00:49:41 +0100

    some changes

--- listings/IDS.cc
+++ listings/IDS.cc
@@ -1,15 +1,18 @@
-#include <iostream>
 #include <utility>
 
-using namespace std;
-
 // maybe change those values
-// i dont know the real safe values
+// i dont know the real life acceptable values
 // will make it to allow for +-50% of the default values
-pair<int, int> data_rate_peer_to_peer_safe_range(20000000, 60000000);
-pair<int, int> delay_peer_to_peer_safe_range(1, 5);
-pair<int, int> data_rate_cluster_safe_range(5000000, 15000000);
-pair<int, int> delay_cluster_safe_range(1, 5);
+pair<int, int> data_rate_peer_to_peer_acceptable_range(20000000, 60000000);
+pair<int, int> delay_peer_to_peer_acceptable_range(1, 5);
+pair<int, int> data_rate_cluster_acceptable_range(5000000, 15000000);
+pair<int, int> delay_cluster_acceptable_range(1, 5);
+
+// those values will be used to fix them if they are outside the safe range
+int data_rate_peer_to_peer_optimal_value = 40000000;
+int delay_peer_to_peer_optimal_value = 3;
+int data_rate_cluster_optimal_value = 10000000;
+int delay_cluster_optimal_value = 3;
 
 
 bool network_intrusion_detection (
@@ -18,6 +21,7 @@ bool network_intrusion_detection (
     int data_rate_cluster,
     int delay_cluster
 ){    
+    cout << "starting network analysis" << endl;
     int changes_detected = 0;
     // here we can either stop the simulation but in real life thats not a solution
 
@@ -29,35 +33,35 @@ bool network_intrusion_detection (
     // if it is just IDS we can suspend the programm until an admin decides what to do 
     // if it is an IPS we can have it change the values
     if (
-        data_rate_peer_to_peer < data_rate_peer_to_peer_safe_range.first || data_rate_peer_to_peer > data_rate_peer_to_peer_safe_range.second
+        data_rate_peer_to_peer < data_rate_peer_to_peer_acceptable_range.first || data_rate_peer_to_peer > data_rate_peer_to_peer_acceptable_range.second
     ){
         cout << "\t data_rate_peer_to_peer is outside of normal range of values" << endl;
         changes_detected++;
-        data_rate_peer_to_peer = 40000000;
+        data_rate_peer_to_peer = data_rate_peer_to_peer_optimal_value;
     }
 
     if(
-        delay_peer_to_peer < delay_peer_to_peer_safe_range.first || delay_peer_to_peer > delay_peer_to_peer_safe_range.second
+        delay_peer_to_peer < delay_peer_to_peer_acceptable_range.first || delay_peer_to_peer > delay_peer_to_peer_acceptable_range.second
     ){
         cout << "\t delay_peer_to_peer is outside of normal range of values" << endl;
         changes_detected++;
-        delay_peer_to_peer = 3;
+        delay_peer_to_peer = delay_peer_to_peer_optimal_value;
     }
 
     if(
-        data_rate_cluster < data_rate_cluster_safe_range.first || data_rate_cluster > data_rate_cluster_safe_range.second
+        data_rate_cluster < data_rate_cluster_acceptable_range.first || data_rate_cluster > data_rate_cluster_acceptable_range.second
     ){
         cout << "\t data_rate_cluster is outside of normal range of values" << endl;
         changes_detected++;
-        data_rate_cluster = 10000000;
+        data_rate_cluster = data_rate_cluster_optimal_value;
     }
 
     if(
-        delay_cluster < delay_cluster_safe_range.first || delay_cluster > delay_cluster_safe_range.second
+        delay_cluster < delay_cluster_acceptable_range.first || delay_cluster > delay_cluster_acceptable_range.second
     ){
         cout << "\t delay_cluster is outside of normal range of values" << endl;
         changes_detected++;
-        delay_cluster = 3;
+        delay_cluster = delay_cluster_optimal_value;
     }
 
     if(changes_detected != 0){
@@ -67,45 +71,4 @@ bool network_intrusion_detection (
         cout << "ids didnt detect any threats" << endl;
         return 1;
     }
-}
-
-
-int main(){
-    cout << "starting network analysis" << endl;
-
-    //network_intrusion_detection(int data_rate_peer_to_peer, int delay_peer_to_peer, int data_rate_cluster, int delay_cluster)
-    
-    
-    cout << "##############################################" << endl;
-    cout << "attack 1:" << endl;
-    network_intrusion_detection(40000000, 3, 1000000, 100);
-    cout << "##############################################" << endl << endl << endl;
-
-    cout << "##############################################" << endl;
-    cout << "attack 2:" << endl;
-    network_intrusion_detection(1000000, 3, 100, 3);
-    cout << "##############################################" << endl << endl << endl;
-
-    cout << "##############################################" << endl;
-    cout << "attack 3:" << endl;
-    network_intrusion_detection(40000000, 3, 5000000, 50);
-    cout << "##############################################" << endl << endl << endl;
-
-    cout << "##############################################" << endl;
-    cout << "attack 4:" << endl;
-    network_intrusion_detection(1000000, 10, 10000000, 3);
-    cout << "##############################################" << endl << endl << endl;
-
-    cout << "##############################################" << endl;
-    cout << "attack 8:" << endl;
-    network_intrusion_detection(40000000, 3, 2000000, 50);
-    cout << "##############################################" << endl << endl << endl;
-
-    cout << "##############################################" << endl;
-    cout << "attack 9:" << endl;
-    network_intrusion_detection(3000000, 70, 10000000, 3);
-    cout << "##############################################" << endl << endl << endl;
-
-
-    return 0;
 }
