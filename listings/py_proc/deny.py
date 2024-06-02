74a784e57f94b3211214f3ed1414aa7f2ad5d684
csosgic <tempo_github@proton.me>
2024-06-02 01:42:28 +0100

    some changes

--- listings/py_proc/deny.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def allow_traffic(src_ip, src_port, dst_ip, dst_port, protocol):
-    subprocess.run(["iptables", "-A", "INPUT", "-p", protocol, "--src", src_ip, "--sport", str(src_port), "--dst", dst_ip, "--dport", str(dst_port), "-j", "ACCEPT"])
-
-def deny_traffic(src_ip, src_port, dst_ip, dst_port, protocol):
-    subprocess.run(["iptables", "-A", "INPUT", "-p", protocol, "--src", src_ip, "--sport", str(src_port), "--dst", dst_ip, "--dport", str(dst_port), "-j", "DROP"])
